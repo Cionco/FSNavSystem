b7f590fba4ae3c802e71b01789eae5e9e2efc52d
Malte Blank <malte.blank@fs-students.de>
2018-04-26 20:00:06 +0200

    Initial Commit

--- FSNavSystem/src/figuren/Figur.java
+++ FSNavSystem/src/figuren/Figur.java
@@ -1,5 +1,20 @@
 package figuren;
 
-public class Figur {
+public abstract class Figur 
+{
+	private Feld position;
+	private String team;
 
+	public Figur()
+	{
+		
+	}
+	
+	public boolean move(Feld f)
+	{
+		
+	}
+	
+	public abstract boolean checkMovement(Feld f);
+	
 }
