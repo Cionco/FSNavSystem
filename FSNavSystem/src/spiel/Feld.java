d018b0255cc2dac1c62fca0fe250b027c7b29ace
tiyamrasolaminy <tiyamrasolaminy@yahoo.de>
2018-04-26 21:06:17 +0200

    plus getset

--- FSNavSystem/src/spiel/Feld.java
+++ FSNavSystem/src/spiel/Feld.java
@@ -6,8 +6,26 @@ public class Feld {
 
 	private int x;
 	private int y;
-
 	private Figur besetzt;
+	
+	public int getx() {
+		return this.x;
+	}
+
+	public void setx(int x) {
+		this.x =x;
+	}
+	
+	public int gety() {
+		return this.y;
+	}
+
+	public void sety(int y) {
+		this.y =y;
+	}
+	
+	
+	
 	
 	public boolean istBesetzt() {	
 	return besetzt != null;
